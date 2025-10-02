cb3f8e36dae0344673c3b24ad73c6f02eec268be
bstocker <boris.stocker@gmail.com>
2025-10-02 10:25:33 +0200

    Update cercle.c

---
+++ TP1/src/cercle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+double pi = 3.1416;
+double r = 6;  
+    printf("L'air du cercle = %d", pi*(r*r));
+   printf("Le périmètre du cercle = %d", pi*(r+r));
+}
