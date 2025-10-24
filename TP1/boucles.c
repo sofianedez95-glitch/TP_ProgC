fe1e3df6f6095e857111aa5b8b8cc7b7b6b81e47
sofianedez95-glitch <sofianedez95@gmail.com>
2025-10-24 11:44:15 +0200

    Update boucles.c

--- TP1/boucles.c
+++ TP1/boucles.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 5; 
+    
+    printf("Triangle rectangle avec des boucles for (compteur = %d)\n\n", compteur);
+
+    for (int i = 1; i <= compteur; i++) {         
+        for (int j = 1; j <= i; j++) {           
+          
+            if (i == 1) {
+                printf("* ");
+            } else if (i == 2) {
+                if (j == 1 || j == i)
+                    printf("* ");
+                else
+                    printf("* ");  
+            } else if (i == 3 || i == 4) {
+                if (j == 1 || j == i)
+                    printf("* ");
+                else
+                    printf("# ");
+            } else {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
