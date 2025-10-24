e8ae9217bbe8937bc1c28684f261662bde49c2c0
sofianedez95-glitch <sofianedez95@gmail.com>
2025-10-24 10:08:55 +0200

    cercle.c

--- TP1/cercle.c
+++ TP1/cercle.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double rayon, aire, perimetre;
+    const double pi = M_PI;
+
+    rayon = 5.0; 
+
+    aire = pi * rayon * rayon;
+    perimetre = 2 * pi * rayon;
+
+    printf("Rayon du cercle : %.2f\n", rayon);
+    printf("Aire du cercle : %.2f\n", aire);
+    printf("Perimetre du cercle : %.2f\n", perimetre);
+
+    return 0;
+}
 
